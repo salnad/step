2f742fcbe555ec40083d34e8a9310c3a2c70b4f3
Salil <salnad02@gmail.com>
2020-05-28 17:59:39 -0400

    Refactored gol code to use requestAnimationFrames / added a non-functioning cancel button

--- portfolio/src/main/webapp/gameoflife.js
+++ portfolio/src/main/webapp/gameoflife.js
@@ -14,72 +14,53 @@
 
 // CODE FOR GAME OF LIFE
 
-canvas = document.getElementById("mycanvas");
+// Page specific variables
+var canvas = document.getElementById("mycanvas");
 var ctx = canvas.getContext("2d");
-
-function resizeCanvasToDisplaySize(canvas) {
-   // look up the size the canvas is being displayed
-   const width = canvas.clientWidth;
-   const height = canvas.clientHeight;
-
-   // If it's resolution does not match change it
-   if (canvas.width !== width || canvas.height !== height) {
-     canvas.width = width;
-     canvas.height = height;
-     return true;
-   }
-
-   return false;
-}
+var frameID;
 
 resizeCanvasToDisplaySize(canvas);
 
-const SQUARE_SIZE = 10;
-const START_SPAWN_RATE = 0.05;
-var rows = Math.floor(canvas.height / SQUARE_SIZE);
-var cols = Math.floor(canvas.width / SQUARE_SIZE);
+// GOL specific variables
+const SPEED = 200; // in miliseconds
+const CELL_SIZE = 10; // in pixels
+const SEED_SPAWN_RATE = 0.1; // as a ratio
 
+var rows = Math.floor(canvas.height / CELL_SIZE);
+var cols = Math.floor(canvas.width / CELL_SIZE);
+
+// Declare grid for GOL
 var grid = new Array(rows);
 for (var i = 0; i < grid.length; i++) {
 	grid[i] = new Array(cols);
 }
 
-for (var x = 0; x < cols; x++) {
-  for (var y = 0; y < rows; y++) {
-    if (Math.random() < START_SPAWN_RATE)
-        grid[y][x] = true;
-    else
-        grid[y][x] = false;
-  }
-} 
 
-function draw() {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	ctx.fillStyle = "rgba(20, 20, 20, 0.4)";
+// GOL specific helper functions
+function seedGrid() {
     for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
-      // ctx.strokeRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-      if (grid[y][x] === true)
-        ctx.fillRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
-    }
-  } 
+        for (var y = 0; y < rows; y++) {
+            if (Math.random() < SEED_SPAWN_RATE) {
+                grid[y][x] = true;
+            }
+            else {
+                grid[y][x] = false;
+            }
+        }
+    } 
 }
 
-function update() {
+function updateGrid() {
   resizeCanvasToDisplaySize(canvas);
   var future = new Array(rows);
   for (var i = 0; i < future.length; i++) {
     future[i] = new Array(cols);
+    for (var j = 0; j < cols; j++) {
+        future[i][j] = false;
+    }
   }
   
-  for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
-      future[y][x] = false;
-    }
-  } 
-  
-  
-	for (var x = 1; x < cols - 1; x++) {
+  for (var x = 1; x < cols - 1; x++) {
   	for (var y = 1; y < rows - 1; y++) {
     	var live_count = 0;
     	var is_alive = grid[y][x] === true;
@@ -107,8 +88,50 @@ function update() {
 			grid[y][x] = future[y][x];
 		}
   } 
-  draw();
 }
 
-// loads the first element in the list in the content on the page loading
-window.onload = setInterval(update, 100);
+function drawGrid() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = "rgba(20, 20, 20, 1)";
+    // ctx.fillStyle = "white";
+    for (var x = 0; x < cols; x++) {
+        for (var y = 0; y < rows; y++) {
+        // ctx.strokeRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+        if (grid[y][x] === true)
+            ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+        }
+    } 
+}
+
+function resizeCanvasToDisplaySize(canvas) {
+   // look up the size the canvas is being displayed
+   const width = canvas.clientWidth;
+   const height = canvas.clientHeight;
+
+   // If it's resolution does not match change it
+   if (canvas.width !== width || canvas.height !== height) {
+     canvas.width = width;
+     canvas.height = height;
+     return true;
+   }
+
+   return false;
+}
+
+function golLoop() {
+    setTimeout(() => {
+        updateGrid();
+        drawGrid();
+        console.log(grid);
+        frameID = requestAnimationFrame(golLoop);
+    }, SPEED);    
+}
+
+seedGrid();
+drawGrid();
+frameID = requestAnimationFrame(golLoop);
+
+function cancel() {
+    cancelAnimationFrame(frameID);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
