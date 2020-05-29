25e5b7af8bc6d34435a13478c1899039609f0191
Salil <salnad02@gmail.com>
2020-05-29 14:58:46 -0400

    add description of gol, used let over var, renamed and reordered functions to be more readable, removed debugging lines

--- portfolio/src/main/webapp/gameoflife.js
+++ portfolio/src/main/webapp/gameoflife.js
@@ -1,46 +1,73 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+/* 
+CODE FOR GAME OF LIFE
+    The game of life is a "cellular automota" game, where a grid of cells which can either be "alive" or "dead".
+    Each game step, all the cells are updated based upon the amount live cells around them (either becoming alive, staying alive, or dying).
+    This code contains the logic for the game of life, and displays it in a canvas element. 
+*/
 
-// CODE FOR GAME OF LIFE
-
-// Page specific variables
-var canvas = document.getElementById("mycanvas");
-var ctx = canvas.getContext("2d");
-var stop = true;
-var interval, startTime, now, then, elapsed;
+// Animation specific variables
+let canvas = document.getElementById("mycanvas");
+let ctx = canvas.getContext("2d");
+let stop = true;
+let interval, startTime, now, then, elapsed;
 
 resizeCanvasToDisplaySize(canvas);
 
-// GOL specific variables
-const SPEED = 200; // in miliseconds
-const CELL_SIZE = 10; // in pixels
-const SEED_SPAWN_RATE = 0.1; // as a ratio
+// Game of Life specific variables
+const SPEED = 200; // in miliseconds (speed between updates)
+const CELL_SIZE = 10; // in pixels (side length of individual cell)
+const SEED_SPAWN_RATE = 0.1; // as a ratio (likelihood a cell is 'alive' when seeded)
 
-var rows = Math.floor(canvas.height / CELL_SIZE);
-var cols = Math.floor(canvas.width / CELL_SIZE);
+let rows = Math.floor(canvas.height / CELL_SIZE);
+let cols = Math.floor(canvas.width / CELL_SIZE);
 
-// Declare grid for GOL
-var grid = new Array(rows);
-for (var i = 0; i < grid.length; i++) {
+// Declare grid for Game of Life
+let grid = new Array(rows);
+for (let i = 0; i < grid.length; i++) {
 	grid[i] = new Array(cols);
 }
 
+// Animation "on/off" function (toggles whether game of life is running or not)
+function toggleGameOfLifeCycle() {
+    resizeCanvasToDisplaySize(canvas);
+    if (stop) {
+        stop = false;
+        startGameOfLifeCycle();
+    } else {
+        stop = true;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+}
 
-// GOL specific helper functions
+function startGameOfLifeCycle() {
+    seedGrid();
+    drawGrid();
+    
+    then = Date.now();
+    startTime = then;
+    gameOfLifeCycle();
+}
+
+function gameOfLifeCycle() {
+    if (stop) {
+        return;
+    }
+    
+    requestAnimationFrame(gameOfLifeCycle);
+    now = Date.now();
+    elapsed = now - then;
+
+    if (elapsed > SPEED) {
+        updateGrid();
+        drawGrid();
+        then = Date.now();
+    }
+}
+
+// Game of Life specific helper functions
 function seedGrid() {
-    for (var x = 0; x < cols; x++) {
-        for (var y = 0; y < rows; y++) {
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
             if (Math.random() < SEED_SPAWN_RATE) {
                 grid[y][x] = true;
             }
@@ -53,27 +80,25 @@ function seedGrid() {
 
 function updateGrid() {
   resizeCanvasToDisplaySize(canvas);
-  var future = new Array(rows);
-  for (var i = 0; i < future.length; i++) {
+  let future = new Array(rows);
+  for (let i = 0; i < future.length; i++) {
     future[i] = new Array(cols);
-    for (var j = 0; j < cols; j++) {
+    for (let j = 0; j < cols; j++) {
         future[i][j] = false;
     }
   }
   
-  for (var x = 1; x < cols - 1; x++) {
-  	for (var y = 1; y < rows - 1; y++) {
-    	var live_count = 0;
-    	var is_alive = grid[y][x] === true;
-      for (var i = -1; i <= 1; i++) {
-      	for (var j = -1; j <= 1; j++) {
+  for (let x = 1; x < cols - 1; x++) {
+  	for (let y = 1; y < rows - 1; y++) {
+    	let live_count = 0;
+    	let is_alive = grid[y][x] === true;
+      for (let i = -1; i <= 1; i++) {
+      	for (let j = -1; j <= 1; j++) {
         	if (grid[y + j][x + i] === true) live_count++;
         }
       }
       
       if (is_alive) live_count--;
-      
-      // console.log(`${x}, ${y}: ${live_count}`);
       
       if (is_alive && (live_count === 2 || live_count === 3)) 
       	future[y][x] = true;
@@ -84,8 +109,8 @@ function updateGrid() {
     }
   }
   
-  for (var x = 0; x < cols; x++) {
-    for (var y = 0; y < rows; y++) {
+  for (let x = 0; x < cols; x++) {
+    for (let y = 0; y < rows; y++) {
 			grid[y][x] = future[y][x];
 		}
   } 
@@ -94,16 +119,15 @@ function updateGrid() {
 function drawGrid() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 	ctx.fillStyle = "rgba(20, 20, 20, 1)";
-    // ctx.fillStyle = "white";
-    for (var x = 0; x < cols; x++) {
-        for (var y = 0; y < rows; y++) {
-        // ctx.strokeRect(x * SQUARE_SIZE, y * SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE);
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
         if (grid[y][x] === true)
             ctx.fillRect(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
         }
     } 
 }
 
+// For styling (ensures canvas resolution is same as webpage)
 function resizeCanvasToDisplaySize(canvas) {
    // look up the size the canvas is being displayed
    const width = canvas.clientWidth;
@@ -117,40 +141,4 @@ function resizeCanvasToDisplaySize(canvas) {
    }
 
    return false;
-}
-
-function startLoop() {
-    seedGrid();
-    drawGrid();
-    
-    then = Date.now();
-    startTime = then;
-    golLoop();
-    // requestAnimationFrame(golLoop);
-}
-
-function golLoop() {
-    if (stop) {
-        return;
-    }
-    
-    requestAnimationFrame(golLoop);
-    now = Date.now();
-    elapsed = now - then;
-
-    if (elapsed > SPEED) {
-        updateGrid();
-        drawGrid();
-        then = Date.now();
-    }
-}
-
-function toggle() {
-    if (stop) {
-        stop = false;
-        startLoop();
-    } else {
-        stop = true;
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
 }
