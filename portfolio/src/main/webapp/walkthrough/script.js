aa0286dd6c954c49cc753468674314bcc47a66b8
Salil <salnad02@gmail.com>
2020-06-04 14:33:32 +0000

    Edit fetch request to take comment_limit as parameter, retrieve comment limit from number input, clear comments upon each call to function

--- portfolio/src/main/webapp/walkthrough/script.js
+++ portfolio/src/main/webapp/walkthrough/script.js
@@ -1,7 +1,9 @@
 async function fetchWalkthroughData() {
-    const fetchedDataResponse = await fetch("/data?comment_limit=1");
+    const commentLimit = document.getElementById("comment_limit").value;
+    const fetchedDataResponse = await fetch(`/data?comment_limit=${commentLimit}`);
     const fetchedDataJSON = await fetchedDataResponse.json();
     const contentList = document.getElementById("walkthrough-content");
+    contentList.innerHTML = "";
     for (let i = 0; i < fetchedDataJSON.length; i++) {
         let listItem = document.createElement("li");
         listItem.innerText = fetchedDataJSON[i];
