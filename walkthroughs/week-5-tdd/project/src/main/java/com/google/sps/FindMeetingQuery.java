8e7765c3b7731be84148367cbe48d5d4ae32a413
Salil <salnad02@gmail.com>
2020-06-15 18:41:41 +0000

    Restructured code to be more readable, ran Java formatter as well (last commit was accidental, meant to commit this)

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,80 +14,116 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.ArrayList;
-import java.util.HashSet;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    ArrayList<TimeRange> conflicts = new ArrayList<TimeRange>();
-    for (Event e : events) {
-      if (!Collections.disjoint(e.getAttendees(), request.getAttendees())) {
-        conflicts.add(e.getWhen());
-      }
-    }
 
-    // System.out.println("BEFORE");
-    // for (TimeRange t : conflicts) {
-    //   System.out.println(t.toString());
-    // }
+  // Takes in a collection of all events (each with seperate attendees and timeranges) and
+  // 'MeetingRequest' (with required attendees and a duration requirement) and returns a Collection
+  // of TimeRanges that work with schedules of required attendees and meets the duration requirement
+  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+    Collection<TimeRange> result;
+
+    ArrayList<TimeRange> conflicts = generateConflicts(events, request.getAttendees());
 
     if (conflicts.isEmpty()) {
-      Collection<TimeRange> res = new ArrayList<TimeRange>();
-      TimeRange possible = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
-      if (possible.duration() >= request.getDuration()) {
-        res.add(possible);
+      // If no conflicts, create an time range for entire day and add to result if possible (meets
+      // duration requirement)
+      result = new ArrayList<TimeRange>();
+      TimeRange fullDay =
+          TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
+      addIfPossible(result, fullDay, request.getDuration());
+    } else {
+      // if conflicts exist, generate free time ranges taking into account conflicts / minimum
+      // duration
+      result = generateFreeTimeRanges(conflicts, request.getDuration());
+    }
+    return result;
+  }
+
+  // Generates arraylist of conflicting events (events attendees of requested meeting are also
+  // attending)
+  private ArrayList<TimeRange> generateConflicts(
+      Collection<Event> events, Collection<String> attendees) {
+    ArrayList<TimeRange> conflicts = new ArrayList<TimeRange>();
+
+    for (Event possibleConflict : events) {
+      // checks if set of attendees of the current 'possible conflicting event' and attendees to
+      // requested event are disjoint (no shared members)
+      if (!Collections.disjoint(possibleConflict.getAttendees(), attendees)) {
+        // adds to conflicts if not disjoint (member in possible conflict is attending requested
+        // meeting)
+        conflicts.add(possibleConflict.getWhen());
       }
-      return res;
     }
 
+    return conflicts;
+  }
+
+  private ArrayList<TimeRange> generateFreeTimeRanges(
+      ArrayList<TimeRange> conflicts, long duration) {
+    ArrayList<TimeRange> freeTimes = new ArrayList<TimeRange>();
+
+    // merge overlapping / contained conflicts into each other, and order them by starting time
+    mergeConflicts(conflicts);
+
+    // generate TimeRange from start of day to start of first event, and add if possible
+    TimeRange startToFirst =
+        TimeRange.fromStartEnd(TimeRange.START_OF_DAY, conflicts.get(0).start(), false);
+    addIfPossible(freeTimes, startToFirst, duration);
+
+    // loop through pairs of adjacent conflicts, create time ranges between end of first and
+    // beginning of second, and add to free times if possible
+    int idx = 0;
+    while (idx < conflicts.size() - 1) {
+      TimeRange current = conflicts.get(idx);
+      TimeRange next = conflicts.get(idx + 1);
+      TimeRange possibleRange = TimeRange.fromStartEnd(current.end(), next.start(), false);
+      addIfPossible(freeTimes, possibleRange, duration);
+      idx++;
+    }
+
+    // generate TimeRange from end of last event to end of day, and add if possible
+    TimeRange lastToEnd =
+        TimeRange.fromStartEnd(
+            conflicts.get(conflicts.size() - 1).end(), TimeRange.END_OF_DAY, true);
+    addIfPossible(freeTimes, lastToEnd, duration);
+
+    return freeTimes;
+  }
+
+  // Merge conflicts (if there's overlap / one conflict contains another, merges into singular
+  // conflict)
+  private void mergeConflicts(ArrayList<TimeRange> conflicts) {
+    // Sort by starting time
     Collections.sort(conflicts, TimeRange.ORDER_BY_START);
-    int curr = 0;
-    while (curr < conflicts.size() - 1) {
-      TimeRange current = conflicts.get(curr);
-      TimeRange next = conflicts.get(curr+1);
-      
+
+    int idx = 0;
+    while (idx < conflicts.size() - 1) {
+      TimeRange current = conflicts.get(idx);
+      TimeRange next = conflicts.get(idx + 1);
       if (current.overlaps(next)) {
-        TimeRange merged = TimeRange.fromStartEnd(current.start(), Math.max(current.end(), next.end()), false);
-        conflicts.remove(curr+1);
-        conflicts.set(curr,merged);
+        // if overlap between current and next event, 'merge' them (remove prev two events and
+        // create new event starting at first events start time, and the maximum of the two events
+        // ending time)
+        TimeRange merged =
+            TimeRange.fromStartEnd(current.start(), Math.max(current.end(), next.end()), false);
+        conflicts.remove(idx + 1);
+        conflicts.set(idx, merged);
       } else {
-        curr++;
+        // otherwise, move onto next event
+        idx++;
       }
     }
+  }
 
-    Collection<TimeRange> result = new HashSet<TimeRange>();
-    if (!conflicts.isEmpty()) {
-      TimeRange startToFirst = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, conflicts.get(0).start(), false);
-      if (startToFirst.duration() >= request.getDuration()) {
-        result.add(startToFirst);
-      }
-      curr = 0;
-      while (curr < conflicts.size() - 1) {
-        TimeRange current = conflicts.get(curr);
-        TimeRange next = conflicts.get(curr + 1);
-        TimeRange possibleRange = TimeRange.fromStartEnd(current.end(), next.start(), false);
-        if (possibleRange.duration() >= request.getDuration()) {
-          result.add(possibleRange);
-        }
-        curr++;
-      }
-      TimeRange lastToEnd = TimeRange.fromStartEnd(conflicts.get(conflicts.size() - 1).end(), TimeRange.END_OF_DAY, true);
-      if (lastToEnd.duration() >= request.getDuration()) {
-        result.add(lastToEnd);
-      }
+  // Checks if time range is valid (satisfies minimum duration requirement), and adds to collection
+  // of TimeRanges if possible
+  private void addIfPossible(Collection<TimeRange> collection, TimeRange range, long duration) {
+    if (range.duration() >= duration) {
+      collection.add(range);
     }
-    ArrayList<TimeRange> res = new ArrayList<TimeRange>(result);
-    Collections.sort(res, TimeRange.ORDER_BY_START);
-    return res;
-
-
-    // System.out.println("AFTER");
-    // for (TimeRange t : conflicts) {
-    //   System.out.println(t.toString());
-    // }
-    
-    // throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
