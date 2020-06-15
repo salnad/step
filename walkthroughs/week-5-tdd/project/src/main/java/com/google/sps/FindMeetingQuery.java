4ceec455b15c8d31fa6914e687e0e9089f4d1256
Salil <salnad02@gmail.com>
2020-06-15 13:57:47 +0000

    Working version of query() implemented

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,79 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> conflicts = new ArrayList<TimeRange>();
+    for (Event e : events) {
+      if (!Collections.disjoint(e.getAttendees(), request.getAttendees())) {
+        conflicts.add(e.getWhen());
+      }
+    }
+
+    // System.out.println("BEFORE");
+    // for (TimeRange t : conflicts) {
+    //   System.out.println(t.toString());
+    // }
+
+    if (conflicts.isEmpty()) {
+      Collection<TimeRange> res = new ArrayList<TimeRange>();
+      TimeRange possible = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true);
+      if (possible.duration() >= request.getDuration()) {
+        res.add(possible);
+      }
+      return res;
+    }
+
+    Collections.sort(conflicts, TimeRange.ORDER_BY_START);
+    int curr = 0;
+    while (curr < conflicts.size() - 1) {
+      TimeRange current = conflicts.get(curr);
+      TimeRange next = conflicts.get(curr+1);
+      
+      if (current.overlaps(next)) {
+        TimeRange merged = TimeRange.fromStartEnd(current.start(), Math.max(current.end(), next.end()), false);
+        conflicts.remove(curr+1);
+        conflicts.set(curr,merged);
+      } else {
+        curr++;
+      }
+    }
+
+    Collection<TimeRange> result = new HashSet<TimeRange>();
+    if (!conflicts.isEmpty()) {
+      TimeRange startToFirst = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, conflicts.get(0).start(), false);
+      if (startToFirst.duration() >= request.getDuration()) {
+        result.add(startToFirst);
+      }
+      curr = 0;
+      while (curr < conflicts.size() - 1) {
+        TimeRange current = conflicts.get(curr);
+        TimeRange next = conflicts.get(curr + 1);
+        TimeRange possibleRange = TimeRange.fromStartEnd(current.end(), next.start(), false);
+        if (possibleRange.duration() >= request.getDuration()) {
+          result.add(possibleRange);
+        }
+        curr++;
+      }
+      TimeRange lastToEnd = TimeRange.fromStartEnd(conflicts.get(conflicts.size() - 1).end(), TimeRange.END_OF_DAY, true);
+      if (lastToEnd.duration() >= request.getDuration()) {
+        result.add(lastToEnd);
+      }
+    }
+    ArrayList<TimeRange> res = new ArrayList<TimeRange>(result);
+    Collections.sort(res, TimeRange.ORDER_BY_START);
+    return res;
+
+
+    // System.out.println("AFTER");
+    // for (TimeRange t : conflicts) {
+    //   System.out.println(t.toString());
+    // }
+    
+    // throw new UnsupportedOperationException("TODO: Implement this method.");
   }
 }
